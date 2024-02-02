db580730c34cfb3f1765ece17cc6bf2bf034f979
sagar <nama.gupta@globallogic.com>
2024-02-02 12:43:44 +0530

    Add auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            Ask Question
+        </div>
+    );
+};
+
+export default Page;
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-      <div>
-        <h1 className="h2-bold">Hello</h1>
-      </div>
-  );
-}
