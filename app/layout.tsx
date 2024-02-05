6dfc19d7066630b10de1b7246592740c00cf2c1c
sagar <nama.gupta@globallogic.com>
2024-02-05 20:41:51 +0530

    Added import statement

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 // eslint-disable-next-line camelcase
 import { Inter, Space_Grotesk } from "next/font/google";
 import { ReactNode } from "react";
+import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/context/ThemeProvider";
 
