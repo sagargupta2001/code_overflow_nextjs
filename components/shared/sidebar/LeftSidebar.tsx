158c53171cd870f72aadd8ee882f163733494cbf
sagar <nama.gupta@globallogic.com>
2024-02-14 13:02:35 +0530

    Major changes

--- components/shared/sidebar/LeftSidebar.tsx
+++ components/shared/sidebar/LeftSidebar.tsx
@@ -1,28 +1,29 @@
 "use client";
+
 import { sidebarLinks } from "@/constants";
+import { SignedOut, useAuth } from "@clerk/nextjs";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import React from "react";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import { SignedOut } from "@clerk/nextjs";
+import {Button} from "@/components/ui/button";
 
 const LeftSidebar = () => {
+    const { userId } = useAuth();
     const pathname = usePathname();
     return (
-        <section
-            className="background-light900_dark200 light-border
-      custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between
-      overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]"
-        >
+        <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between gap-6 overflow-y-auto border-r p-6 pt-36 shadow-light-300 max-sm:hidden lg:w-[266px] dark:shadow-none">
             <div className="flex flex-1 flex-col gap-6">
                 {sidebarLinks.map((item) => {
                     const isActive =
                         (pathname.includes(item.route) && item.route.length > 1) ||
                         pathname === item.route;
-
-                    // TODO profile routes
-
+                    if (item.route === "/profile") {
+                        if (userId) {
+                            item.route = `${item.route}/${userId}`;
+                        } else {
+                            return null;
+                        }
+                    }
                     return (
                         <Link
                             key={item.route}
@@ -51,40 +52,30 @@ const LeftSidebar = () => {
                     );
                 })}
             </div>
-
             <SignedOut>
                 <div className="flex flex-col gap-3">
                     <Link href="/sign-in">
-                        <Button
-                            className="small-medium btn-secondary
-                    min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
-                        >
+                        <Button className="small-medium btn-secondary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
                             <Image
-                                src="assets/icons/account.svg"
-                                alt="login"
+                                src="/assets/icons/account.svg"
+                                alt="Login"
                                 width={20}
                                 height={20}
                                 className="invert-colors lg:hidden"
                             />
-                            <span className="primary-text-gradient max-lg:hidden">
-                Log In
-              </span>
+                            <span className="primary-text-gradient max-lg:hidden">Login</span>
                         </Button>
                     </Link>
-
                     <Link href="/sign-up">
-                        <Button
-                            className="small-medium light-border-2 btn-tertiary text-dark400_light900
-                    min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none"
-                        >
+                        <Button className="small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none">
                             <Image
-                                src="assets/icons/sign-up.svg"
-                                alt="signup"
+                                src="/assets/icons/sign-up.svg"
+                                alt="Sign Up"
                                 width={20}
                                 height={20}
                                 className="invert-colors lg:hidden"
                             />
-                            <span className="max-lg:hidden">Sign up</span>
+                            <span className="max-lg:hidden">Sign Up</span>
                         </Button>
                     </Link>
                 </div>
